95e5f77cb614dad4bb64c0384dbf0d8b8d8f8f59
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-10-01 13:51:20 +0200

    header styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,11 @@ import Image from "next/image"
 
 const Header = () => {
   return (
-    <div className='flex text-black text-2xl h-20 justify-center items-center font-semibold underline decoration-2 underline-offset-4'>
+    <div className='flex text-black h-20 justify-center items-end mb-16'>
       <div className="absolute left-2 p-2">
-        <Image src={Logo} alt="logo" role="img"/>
+        <Image src={Logo} alt="logo" role="img" />
       </div>
-      <h1>Memorista</h1>
+      <h1 className="text-4xl font-semibold font-mono p-1">Memorista</h1>
     </div>
   )
 }
